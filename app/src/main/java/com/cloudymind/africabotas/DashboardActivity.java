bd9c0fdc77fe4a2e176998486efb7f9e4144d050
Roberto Aguilera <rc7.aguilera@gmail.com>
2022-03-30 16:23:40 -0600

    añadiendo dashboard

---
+++ app/src/main/java/com/cloudymind/africabotas/DashboardActivity.java
@@ -0,0 +1,20 @@
+package com.cloudymind.africabotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cloudymind.africabotas.databinding.ActivityDashboardBinding;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private ActivityDashboardBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
